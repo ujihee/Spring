ab0d8dec2ebdd96ef19e4b3c4cb7cad608eba54b
dream1910 <dream1910@naver.com>
2025-09-15 10:55:54 +0900

    new

---
+++ ch01/src/main/java/kr/co/ch01/config/AppConfig.java
@@ -0,0 +1,20 @@
+package kr.co.ch01.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"kr.co.ch01"})
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views", ".jsp");
+//views/ 뒤에 /빼줌 maincontroller에서 /hello로 되어있기 때문에
+
+    }
+}
